e4aee8e65f087ac3c195ebf399ddd5a94248cdc3
js Kim <behappy0916@naver.com>
2022-07-23 12:03:34 +0900

    test

---
+++ src/이코테/Q02.java
@@ -0,0 +1,29 @@
+package ภฬฤฺลื;
+
+import java.util.Scanner;
+
+public class Q02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String n = input.next();
+		int a=0;
+		int result = 0;
+		for(int i=0; i<n.length(); i++) {
+			a = (Integer.valueOf(n.charAt(i)-'0'));
+			System.out.println(a+2);
+			if(a<=1 || result<=1) {
+				result +=a;
+			}
+			else {
+				result *=a;
+				
+			}
+			
+		}
+		System.out.println(result);
+
+	}
+
+}
